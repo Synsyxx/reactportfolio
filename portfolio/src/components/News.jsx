2f1f5e12c8d185f80fd43ec8f3a6e6a054603143
synsyxx <travis.simpson92@hotmail.com>
2018-05-16 20:54:56 -0500

    Boiler plate started

---
+++ portfolio/src/components/News.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css';
+
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                NEWS
+            </div>
+        )
+    }
+}
